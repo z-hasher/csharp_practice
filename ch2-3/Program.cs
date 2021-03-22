ce40338effd7cea90b44c2afccff64e663541eb1
Weixiong Zheng <wzheng@carbon3d.com>
2021-03-22 00:15:07 -0700

    restructure

---
+++ ch2-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstCSharp {
+    class ClassOne  {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello?");
+            int a = 10;
+            decimal b = 5.1M;
+            double c = 2.2;
+            char d = 'a';
+            a /= 2;
+            a++;
+            a %= 4;
+            int e = (int)b;
+            Console.WriteLine(a);
+            Console.WriteLine(e);
+            Console.Read();
+        }
+    }
+}
